fa795d74773cec050f858494261e885fcb4293a2
Ouss84 <bahrioussama84@gmail.com>
2022-03-30 12:51:21 +0300

    restructuring the pages, setting navigation and styling the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import Input from "./components/input/Input";
+import Dashboard from "./pages/dashboard/Dashboard";
+import LandingPage from "./pages/landingPage/LandingPage";
+import Header from "./components/header/Header";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Dreamteam phz project
-      <Input />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
