9b98c1a096c3ea4a4887d34dbfd74e37bc1249e0
Ouss84 <bahrioussama84@gmail.com>
2022-03-30 18:38:35 +0300

    styling form button

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,15 +1,20 @@
 import React from "react";
-import styles from "./Form.module.css"
+import styles from "./Form.module.css";
 
 const Form = () => {
   return (
     <div className={styles.main}>
       <div className={styles.title}>
         <h1>NPS Survey Form</h1>
-        <p>Questions marked with <sup>*</sup> are required</p>
+        <p>
+          Questions marked with <sup>*</sup> are required
+        </p>
       </div>
       <div className={styles.question}>
-        <p>Would you recomend PHZ Full Stack for your friends as an employer?<sup>*</sup></p>
+        <p>
+          Would you recomend PHZ Full Stack for your friends as an employer?
+          <sup>*</sup>
+        </p>
       </div>
 
       <div className={styles.tool}>
@@ -24,13 +29,13 @@ const Form = () => {
         <input type="radio" id="5" name="rad" value="5" />
         <label for="5">5</label>
         <input type="radio" id="6" name="rad" value="6" />
-        <label for="6" >6</label>
+        <label for="6">6</label>
         <input type="radio" id="7" name="rad" value="7" />
-        <label for="7" >7</label>
+        <label for="7">7</label>
         <input type="radio" id="8" name="rad" value="8" />
-        <label for="8" >8</label>
+        <label for="8">8</label>
         <input type="radio" id="9" name="rad" value="9" />
-        <label for="9" >9</label>
+        <label for="9">9</label>
         <input type="radio" id="10" name="rad" value="10" />
         <label for="10">10</label>
       </div>
@@ -39,14 +44,15 @@ const Form = () => {
         <p>extreamly likely</p>
       </div>
       <div className={styles.precomment}>
-        <p>What is the main reason for your score?<sup>*</sup></p>
+        <p>
+          What is the main reason for your score?<sup>*</sup>
+        </p>
       </div>
       <textarea name="message" rows="6" cols="30" />
-      
-      <button>Submit</button>
-      
-    </div >
-  )
+
+      <button className={styles.button}>Submit</button>
+    </div>
+  );
 };
 
 export default Form;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Dashboard.module.css";
+import Form from "../../components/form/Form";
 const Dashboard = () => {
-  return <div className={styles.main}>Dashboard</div>;
+  return (
+    <div className={styles.main}>
+      <Form />
+    </div>
+  );
 };
 
 export default Dashboard;
