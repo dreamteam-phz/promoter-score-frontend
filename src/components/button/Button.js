3d2261104b2980af692167298a694179e5f5ec5d
abel-parada <abel.parada.millan@gmail.com>
2022-03-30 15:45:05 +0300

    created:landing page

---
+++ src/components/button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Button.module.css'
+
+const Button = (props) => {
+    return (
+        <div>
+            <button>BUTTON</button>
+        </div>
+    );
+};
+
+export default Button;
+
+
