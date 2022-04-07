48deb213ef3cc818052af8ded0eb44f994e2ae93
Ouss84 <bahrioussama84@gmail.com>
2022-04-07 18:24:13 +0100

    adding a notification bar to the form creatino and form submission, extracting the link to the form

--- src/Home/Create.js
+++ src/Home/Create.js
@@ -2,9 +2,18 @@ import axios from "axios";
 import React, { useState } from "react";
 import styles from "./Create.module.css";
 import Label from "./Label";
+import IconButton from "@material-ui/core/IconButton";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default function Create() {
   const [data, setData] = useState({ question: "", comment: true });
+  const [linkToForm, setLinkToForm] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleToClose = (event, reason) => {
+    if ("clickaway" === reason) return;
+    setOpen(false);
+  };
   const inputHandler = (event) => {
     setData({ ...data, [event.target.name]: event.target.value });
   };
@@ -15,40 +24,85 @@ export default function Create() {
         question: data.question,
         comment: data.comment ? true : false,
       })
-      .then((res) => console.log(res));
+      .then((res) => {
+        console.log(res);
+        setLinkToForm(res.data._id);
+      });
     setData({ question: "" });
+    setOpen(true);
   };
-
-  return (
-    <div className={styles.create}>
-      <div className={styles.data}>
-        <h1>CREATE SURVEY</h1>
-        <input
-          name="question"
-          type="text"
-          placeholder="Enter your question"
-          onChange={inputHandler}
-          value={data.question}
-        />
-        <div className={styles.container}>
-          <div className={styles.comment}>
-            <Label
-              id="on"
-              name="comment"
-              checked={true}
-              content="on"
-              change={inputHandler}
-            />
-            <Label
-              id="off"
-              name="comment"
-              content="off"
-              change={inputHandler}
+  if (linkToForm === "") {
+    return (
+      <div className={styles.create}>
+        <div className={styles.data}>
+          <h1>CREATE SURVEY</h1>
+          <input
+            name="question"
+            type="text"
+            placeholder="Enter your question"
+            onChange={inputHandler}
+            value={data.question}
+          />
+          <div className={styles.container}>
+            <div className={styles.comment}>
+              <Label
+                id="on"
+                name="comment"
+                checked={true}
+                content="on"
+                change={inputHandler}
+              />
+              <Label
+                id="off"
+                name="comment"
+                content="off"
+                change={inputHandler}
+              />
+            </div>
+            <button onClick={submitHandler}>SUBMIT</button>
+            <Snackbar
+              anchorOrigin={{
+                horizontal: "center",
+                vertical: "top",
+              }}
+              open={open}
+              autoHideDuration={3000}
+              message="Survey created"
+              onClose={handleToClose}
+              action={
+                <React.Fragment>
+                  <IconButton
+                    size="small"
+                    aria-label="close"
+                    color="inherit"
+                    onClick={handleToClose}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </IconButton>
+                </React.Fragment>
+              }
             />
           </div>
-          <button onClick={submitHandler}>SUBMIT</button>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.formLink}>
+        <label>Link to the form:</label>
+        <span
+          className={styles.span}
+        >{`http://localhost:3000/${linkToForm}`}</span>
+        <button className={styles.button}>
+          <a
+            href={`http://localhost:3000/${linkToForm}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Link to the form
+          </a>
+        </button>
+      </div>
+    );
+  }
 }
--- src/Home/Form.js
+++ src/Home/Form.js
@@ -1,83 +1,133 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Form.module.css";
 import Label from "../components/form/Label";
-import axios from 'axios';
+import axios from "axios";
 import { useParams } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Form = () => {
-  const [form, setForm] = useState({score: '', comment: ''});
+  const [form, setForm] = useState({ score: "", comment: "" });
+  const [open, setOpen] = useState(false);
+  const handleToClose = (event, reason) => {
+    if ("clickaway" === reason) return;
+    setOpen(false);
+  };
   const [survey, setSurvey] = useState({
-    question: 'Would you recomend PHZ Full Stack for your friends as an employer?',
-    comment: true
-  })
+    question:
+      "Would you recomend PHZ Full Stack for your friends as an employer?",
+    comment: true,
+  });
   const params = useParams();
 
   useEffect(() => {
-    axios.get('http://localhost:4000/api/surveys/' + params.id).then(response => {
-      console.log(response.data)
-      setSurvey(response.data);
-    })
-  },[]);
+    axios
+      .get("http://localhost:4000/api/surveys/" + params.id)
+      .then((response) => {
+        console.log(response.data);
+        setSurvey(response.data);
+      });
+  }, []);
 
   useEffect(() => {
     getIP();
   }, []);
   const getIP = () => {
-    axios.get('https://api.my-ip.io/ip').then(response => setForm({...form, ip: response.data}));
-  }
-
+    axios
+      .get("https://api.my-ip.io/ip")
+      .then((response) => setForm({ ...form, ip: response.data }));
+  };
 
   const textAreaHandler = (event) => {
-    setForm({...form, [event.target.name]: event.target.value})
-  }
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
   const scoreHandler = (event) => {
-    setForm({...form, [event.target.name]: +event.target.value});
-  }
-  const labelsToDisplay = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
-    return <Label id={item} change={scoreHandler} content={item} />
-  })
+    setForm({ ...form, [event.target.name]: +event.target.value });
+  };
+  const labelsToDisplay = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+    return <Label key={item} id={item} change={scoreHandler} content={item} />;
+  });
   // check console when pressing submit form
   const submitHandler = (event) => {
     event.preventDefault();
-    axios.patch('http://localhost:4000/api/update/' + params.id, {
-      results: [
-        form
-      ]
-    }).then(response => {
-      console.log(response)
-    });
-  }
- 
+    axios
+      .patch("http://localhost:4000/api/update/" + params.id, {
+        results: [form],
+      })
+      .then((response) => {
+        console.log(response);
+      });
+    setForm({ score: "", comment: "" });
+
+    setOpen(true);
+  };
+
   return (
     <>
-    <div className={styles.main}>
-      <div className={styles.title}>
-        <h1>NPS Survey Form</h1>
-        <p>
-          Questions marked with <sup>*</sup> are required
-        </p>
-      </div>
-      <div className={styles.question}>
-        <p>
-          {survey.question}
-          <sup>*</sup>
-        </p>
-      </div>
+      <div className={styles.main}>
+        <div className={styles.title}>
+          <h1>NPS Survey Form</h1>
+          <p>
+            Questions marked with <sup>*</sup> are required
+          </p>
+        </div>
+        <div className={styles.question}>
+          <p>
+            {survey.question}
+            <sup>*</sup>
+          </p>
+        </div>
 
-      <div className={styles.tool}>
-        {labelsToDisplay}
-      </div>
-      <div className={styles.describers}>
-        <p>not likely at all</p>
-        <p>extreamly likely</p>
-      </div>
-      {survey.comment && 
-      <div className={styles.precomment}><p>What is the main reason for your score?<sup>*</sup></p></div>}
-      {survey.comment && 
-      <textarea onChange={textAreaHandler} name="comment" rows="6" cols="30" placeholder="type your message here" />}
+        <div className={styles.tool}>{labelsToDisplay}</div>
+        <div className={styles.describers}>
+          <p>not likely at all</p>
+          <p>extreamly likely</p>
+        </div>
+        {survey.comment && (
+          <div className={styles.precomment}>
+            <p>
+              What is the main reason for your score?<sup>*</sup>
+            </p>
+          </div>
+        )}
+        {survey.comment && (
+          <textarea
+            onChange={textAreaHandler}
+            name="comment"
+            rows="6"
+            cols="30"
+            placeholder="type your message here"
+            value={form.comment}
+          />
+        )}
 
-      <button onClick={submitHandler} className={styles.button}>Submit</button>
-    </div>
+        <button onClick={submitHandler} className={styles.button}>
+          Submit
+        </button>
+        <Snackbar
+          anchorOrigin={{
+            horizontal: "center",
+            vertical: "top",
+          }}
+          open={open}
+          autoHideDuration={3000}
+          message="Thank you for your answer! We appreciate your time!"
+          onClose={handleToClose}
+          action={
+            <React.Fragment>
+              <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleToClose}
+              >
+                <CloseIcon fontSize="small" />
+              </IconButton>
+            </React.Fragment>
+          }
+        />
+      </div>
     </>
   );
 };
