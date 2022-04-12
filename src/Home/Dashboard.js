4c2307da609066f94f019c0644a10abe4d84958f
Ouss84 <bahrioussama84@gmail.com>
2022-04-12 17:01:34 +0100

    installing react-chart + displaying responders chart

--- src/Home/Dashboard.js
+++ src/Home/Dashboard.js
@@ -1,44 +1,72 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+
 import styles from "./Dashboard.module.css";
+import RespondersChart from "./RespondersChart";
 
 export default function Dashboard() {
   const [data, setData] = useState([]);
-
   useEffect(() => {
     axios.get("http://localhost:4000/api/formscores").then((response) => {
-      // setData(response.data);
       setData(response.data[0].results.map((item) => item.score));
-
-      // console.log(response.data);
     });
   }, []);
 
   // NetPromScore logic
+
   let prom = 0;
   let det = 0;
   let pass = 0;
   for (let score of data) {
     if (score >= 9) prom++;
-    else if (score <= 6) det = det + 1;
+    else if (score <= 6) det++;
   }
   pass = data.length - (prom + det);
-  console.log("passives: " + pass);
-  console.log("promoters: " + prom);
-  console.log("detractors: " + det);
-  console.log("Total: " + data.length);
+  // console.log("passives: " + pass);
+  // console.log("promoters: " + prom);
+  // console.log("detractors: " + det);
+  // console.log("Total: " + data.length);
   const result = ((prom - det) / data.length) * 100;
   const promScore = result.toFixed(2);
-  console.log("Score: " + promScore);
 
-  // console.log(data);
-  // data.map((item) => console.log(item.results));
+  // console.log("Score: " + promScore);
+  console.log(promScore);
+
   return (
     <div className={styles.dashboard}>
       <h1>DASHBOARD</h1>
       <div className={styles.data}>
-        {data.length > 0 ? promScore : <p>No data to display</p>}
-        <div>{data.length}</div>
+        <div className={styles.panel}>
+          <div className={styles.resultWraper}>
+            <h3>Promoter Score</h3>
+            <span>
+              {data.length > 0 ? promScore : <p>No data to display</p>}
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.panel}>
+          <div className={styles.respondersWraper}>
+            {data.length > 0 ? (
+              <div>
+                <RespondersChart
+                  promoters={prom}
+                  detractors={det}
+                  passives={pass}
+                />
+                <div className={styles.resultWraper}>
+                  <h4>Total responders</h4>
+                  <span>{data.length}</span>
+                </div>
+              </div>
+            ) : (
+              <div className={styles.resultWraper}>
+                <h4>Total responders</h4>
+                <span>{data.length}</span>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/Home/RespondersChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "chart.js/auto";
+import { Pie } from "react-chartjs-2";
+
+const RespondersChart = ({ promoters, detractors, passives }) => {
+  const labels = ["Promoters", "Detractors", "Passives"],
+    datasets = [
+      {
+        data: [promoters, detractors, passives],
+        backgroundColor: ["#1D4E89", "#0092AE", "#7DCFB6"],
+      },
+    ];
+
+  return (
+    <Pie
+      options={{
+        width: "400",
+        height: "400",
+      }}
+      data={{
+        labels: labels,
+        datasets: datasets,
+      }}
+    />
+  );
+};
+
+export default RespondersChart;
