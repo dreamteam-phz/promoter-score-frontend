f088eedce4b317848b6bf9131b8add6236a0afb9
Ouss84 <bahrioussama84@gmail.com>
2022-04-29 01:07:09 +0300

    cleaning display filter

--- src/Home/Dashboard/DisplayFilter.js
+++ src/Home/Dashboard/DisplayFilter.js
@@ -1,14 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "./DisplayFilter.module.css";
 import axios from "axios";
 
 const DisplayFilter = () => {
-  const period = useSelector(state => state.dashboard.selectedMonth)
+  const period = useSelector((state) => state.dashboard.selectedMonth);
   const dispatch = useDispatch();
 
-  const URL_SURVEY_API = 'http://localhost:4000/api/surveys';
+  const URL_SURVEY_API = "http://localhost:4000/api/surveys";
   const [selectSurvey, setSelectSurvey] = useState([]);
 
   const options = [
@@ -18,41 +18,48 @@ const DisplayFilter = () => {
     { label: "1 year", value: "365" },
   ];
 
-  useEffect (() => { 
-    axios.get(URL_SURVEY_API)
-      .then(response => {
+  useEffect(() => {
+    axios
+      .get(URL_SURVEY_API)
+      .then((response) => {
         const data = response.data;
         setSelectSurvey(data);
       })
-      .catch((error)=> console.log(error.message));
-  },[]);
+      .catch((error) => console.log(error.message));
+  }, []);
 
   const filterChangeHandler = (event) => {
     dispatch({
-      type: 'DASHBOARD',
-      payload: {[event.target.name]: event.target.value}
+      type: "DASHBOARD",
+      payload: { [event.target.name]: event.target.value },
     });
   };
-  
+
   return (
     <div className={styles.selectWrapper}>
-      <select 
-      name="selectedSurvey"
-      value={period} 
-      onChange={filterChangeHandler} 
-      className={styles.select}
+      <select
+        name="selectedSurvey"
+        value={period}
+        onChange={filterChangeHandler}
+        className={styles.select}
       >
-        {selectSurvey.map((survey) => 
-        <option 
-          key={survey._id} 
-          value={survey._id}
-        >
-          {survey.question}
-        </option>)}
-
+        {selectSurvey.map((survey) => (
+          <option key={survey._id} value={survey._id}>
+            {survey.question}
+          </option>
+        ))}
       </select>
-      <select name="selectedMonth" value={period} onChange={filterChangeHandler} className={styles.select}>
-        {options.map(option => <option key={option.value} value={option.value}>{option.label}</option> )}
+      <select
+        name="selectedMonth"
+        value={period}
+        onChange={filterChangeHandler}
+        className={styles.select}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
     </div>
   );
