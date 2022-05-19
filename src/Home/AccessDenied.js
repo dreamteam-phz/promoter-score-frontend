af73939c0099da3519cb554056064f7b3934875b
Saara Leppis <saara@leppihalme.fi>
2022-05-19 16:06:28 +0300

    icon and styling of AccessDenied

--- src/Home/AccessDenied.js
+++ src/Home/AccessDenied.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from "./AccessDenied.module.css";
+import { MdOutlineDoNotDisturbOff } from 'react-icons/md'
 
 const AccessDenied = () => {
     return (
         < div className={styles.cookieMessage}>
+            <div className={styles.deniedIcon}>
+                <MdOutlineDoNotDisturbOff className={styles.disturbIcon} />
+            </div>
             <p className={styles.message}>You have already replied to this survey.
                 <br />Please try again later.
             </p>
