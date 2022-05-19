9d51f8612ca97f9beb05c407a04eac996dcf69b5
Ainozerov Sergei <ainozerie.prod@gmail.com>
2022-05-19 13:34:47 +0300

    useSelector added to Crate.js

--- src/Home/Create.js
+++ src/Home/Create.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import AnotherSurvey from "./AnotherSurvey";
 import styles from "./Create.module.css";
 
