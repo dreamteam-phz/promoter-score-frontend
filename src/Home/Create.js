e0a5dc3d2898bceed33edea7114e642249845997
Ainozerov Sergei <ainozerie.prod@gmail.com>
2022-05-20 14:16:28 +0300

    src updated

--- src/Home/Create.js
+++ src/Home/Create.js
@@ -14,9 +14,9 @@ export default function Create() {
   const api_url = useSelector((state) => state.api_url);
   const URL_SURVEY_API = api_url + 'surveys';
   // const src = 'http://localhost:3000/'; // for local
-  const src = 'https://promoter-score-frontend-ht8wtkb1f-dream-team-backend.vercel.app/'; 
+  const src = 'https://promoter-score-frontend.vercel.app/'; 
   useEffect(() => loadData(), [isLoading]);
-  console.log('src:', src);
+  console.log('updated src:', src);
   const loadData = () => {
     axios.get(URL_SURVEY_API)
       .then(response => {
